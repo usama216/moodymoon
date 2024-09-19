21af0a0b8fd4c2d36d97e0dddf95c7ee13e2d16b
Usama Jawad <usamajatt16@gmail.com>
2024-09-20 02:48:56 +0500

    new updated

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -3,12 +3,12 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
     palette: {
         primary: {
-          main: '#2579e1', 
+          main: '#195630',
         },
         secondary: {
           main: '#2579e1',
         },
-        
+
       },
-    
+
 });
